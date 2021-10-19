092cd8abf9ce04d43b1f03b31a64f90cc35b68ae
Kei Takahashi <ktakahashi.dev@gmail.com>
2021-10-18 23:32:30 -0400

    add lua file

---
+++ lua/fugitive-sidekick.lua
@@ -0,0 +1,37 @@
+local getetVisualSelection = function()
+  local _, startLine, startCol, _ = unpack(vim.fn.getpos("'<"))
+  local _, endLine, endCol, _ = unpack(vim.fn.getpos("'>"))
+  print(startLine, startCol, endLine, endCol)
+
+  if startLine == endLine then
+    local line = vim.api.nvim_buf_get_lines(0, startLine - 1, startLine, true)[1]
+    local selected = string.sub(line, startCol, endCol)
+    print(selected)
+    return selected
+  end
+end
+
+local listFilesInCommit = function()
+  local cmdId = GetVisualSelection()
+  -- todo
+  --   1. check if cmdId is in a form of commit id
+  --   2. check register if cmdId is empty?
+  local difftoolCmd = "tabnew | G difftool --name-only " .. cmdId .. "^ " .. cmdId .. " | Gvdiffsplit !^"
+  -- print(difftoolCmd)
+  vim.cmd(difftoolCmd)
+end
+
+local diffNext = function()
+  vim.cmd("quit | cnext | Gvdiffsplit !^")
+end
+
+local diffPrev = function()
+  vim.cmd("quit | cprev | Gvdiffsplit !^")
+end
+
+return {
+  listFilesInCommit = listFilesInCommit,
+  diffNext = diffNext,
+  diffPrev = diffPrev
+}
+
