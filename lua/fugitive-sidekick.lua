a9178f745b24e3e193125596a13b536c05ba0c91
Kei Takahashi <ktakahashi.dev@gmail.com>
2021-10-19 22:03:32 -0400

    more print

--- lua/fugitive-sidekick.lua
+++ lua/fugitive-sidekick.lua
@@ -1,7 +1,8 @@
 local getVisualSelection = function()
   local _, startLine, startCol, _ = unpack(vim.fn.getpos("'<"))
   local _, endLine, endCol, _ = unpack(vim.fn.getpos("'>"))
-
+  print(startLine, startCol, endLine, endCol)
+  
   if startLine == endLine then
     local line = vim.api.nvim_buf_get_lines(0, startLine - 1, startLine, true)[1]
     if line == nil then
@@ -9,14 +10,16 @@ local getVisualSelection = function()
       print(startLine, startCol, endLine, endCol)
       return
     end
+    print('line', line)
     local selected = string.sub(line, startCol, endCol)
-    -- print(selected)
+    print('selected', selected)
     return selected
   end
 end
 
 local listFilesInCommit = function()
   local cmdId = getVisualSelection()
+  print('cmdId', cmdId)
 
   if cmdId == nil then
     print('cmdId is nil')
@@ -26,7 +29,7 @@ local listFilesInCommit = function()
   --   1. check if cmdId is in a form of commit id
   --   2. check register if cmdId is empty?
   local difftoolCmd = "tabnew | G difftool --name-only " .. cmdId .. "^ " .. cmdId .. " | Gvdiffsplit !^"
-  -- print(difftoolCmd)
+  print('difftoolCmd', difftoolCmd)
   vim.cmd(difftoolCmd)
 end
 
